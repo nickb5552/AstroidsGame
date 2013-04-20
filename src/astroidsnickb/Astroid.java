76171eb0452436226e69318335d058dafc91ed1e
nick <nick@10.0.1.9>
2013-04-20 12:48:34 -0700

    Second Game, with astroids

--- src/astroidsnickb/Astroid.java
+++ src/astroidsnickb/Astroid.java
@@ -1,6 +1,24 @@
 package astroidsnickb;
 
-public class Astroid 
-{
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
 
+public class Astroid
+{
+    Ellipse2D.Double astroid;
+    
+    public Astroid()
+    {
+        astroid = new Ellipse2D.Double(500, 500, 50, 50);
+    }
+
+    public void moveSelf()
+    {
+    }
+
+    public void paintSelf(Graphics2D g2)
+    {
+        g2.fill(astroid);
+    }
 }

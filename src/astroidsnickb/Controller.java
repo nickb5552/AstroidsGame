3c4496142b8b8fc86bd3ffe9f9cfe503cabea9d4
nick <nick@10.0.1.9>
2013-04-26 13:07:00 -0700

    Changed heading calculations to radians...Vic

--- src/astroidsnickb/Controller.java
+++ src/astroidsnickb/Controller.java
@@ -3,17 +3,24 @@ package astroidsnickb;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
-public class Controller extends JComponent implements KeyListener
+public class Controller extends JComponent implements KeyListener, ActionListener
 {
     JFrame astroidField;
     int width = Toolkit.getDefaultToolkit().getScreenSize().width;
     int height = Toolkit.getDefaultToolkit().getScreenSize().height;
     Ship battleCruiser;
+    Timer paintTicker;
+    Timer astroidTicker;
+    ArrayList<Astroid> astroidList;
 
     public static void main(String[] args)
     {
@@ -22,6 +29,11 @@ public class Controller extends JComponent implements KeyListener
 
     private void getGoing()
     {
+        astroidList = new ArrayList<Astroid>();
+        paintTicker = new Timer(20, this);
+        paintTicker.start();
+        astroidTicker = new Timer(1000, this);
+        astroidTicker.start();
         astroidField = new JFrame("Astroids");
         astroidField.setVisible(true);
         astroidField.setSize(width, height);
@@ -30,38 +42,44 @@ public class Controller extends JComponent implements KeyListener
         astroidField.add(this);
         astroidField.addKeyListener(this);
     }
-    
+
     @Override
     public void paint(Graphics g)
     {
-        Graphics2D g2 = (Graphics2D)g;
-        battleCruiser.paintSelf(g2);
+        Graphics2D g2 = (Graphics2D) g;
         battleCruiser.moveSelf();
-        repaint();
+        battleCruiser.paintSelf(g2);
+        for(Astroid a : astroidList) 
+        {
+            a.paintSelf(g2);
+        }
     }
 
     @Override
     public void keyTyped(KeyEvent ke)
     {
-       
     }
 
     @Override
     public void keyPressed(KeyEvent ke)
     {
-        if (ke.getKeyCode() == 37)
+        if (ke.getKeyCode() == 37)//turns left
         {
             battleCruiser.shipHeading -= 5;
         }
-        
-        if (ke.getKeyCode() == 39)
+
+        if (ke.getKeyCode() == 39)//turns right
         {
             battleCruiser.shipHeading += 5;
         }
-        
-        if (ke.getKeyCode() == 38)
+
+        if (ke.getKeyCode() == 38)//increase speed
         {
             battleCruiser.shipSpeed += 1;
+        }
+        if (ke.getKeyCode() == 40)//decrease speed
+        {
+            battleCruiser.shipSpeed -= 1;
         }
     }
 
@@ -69,5 +87,14 @@ public class Controller extends JComponent implements KeyListener
     public void keyReleased(KeyEvent ke)
     {
     }
-}
 
+    @Override
+    public void actionPerformed(ActionEvent ae)
+    {
+        repaint();
+        if (ae.getSource() == astroidTicker)
+        {
+            astroidList.add(new Astroid());
+        }
+    }
+}
--- src/astroidsnickb/Ship.java
+++ src/astroidsnickb/Ship.java
@@ -4,15 +4,19 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Polygon;
+import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
 
 public class Ship
 {
-    int shipXpos = 950;
-    int shipYpos = 800;
-    int shipSpeed = 0;
+    int width = Toolkit.getDefaultToolkit().getScreenSize().width;
+    int height = Toolkit.getDefaultToolkit().getScreenSize().height;
+    double shipXpos = 950;
+    double shipYpos = 800;
+    double shipSpeed = 0;
     double shipDeltaX = 0;
     double shipDeltaY = 0;
-    int shipHeading = 0;
+    double shipHeading = 0;
     int[] xPoints =
     {
         0, 3, 1, 1, -1, -1, -3
@@ -25,19 +29,40 @@ public class Ship
 
     public void moveSelf()
     {
-        shipDeltaX = Math.sin(shipHeading) * shipSpeed;
-        shipDeltaY = -Math.cos(shipHeading) * shipSpeed;
-        shipXpos = (int) (shipXpos + shipDeltaX);
+        shipDeltaX = Math.sin(Math.toRadians(shipHeading)) * shipSpeed;
+        shipDeltaY = -Math.cos(Math.toRadians(shipHeading)) * shipSpeed;
+        shipXpos = shipXpos + shipDeltaX;
         shipYpos += shipDeltaY;
+        if (shipYpos < 0)
+        {
+            shipYpos = height;
+        }
+        
+        if (shipYpos > height)
+        {
+            shipYpos = 0;
+        }
+        
+        if (shipXpos < 0)
+        {
+            shipXpos = width;
+        }
+         if (shipXpos > width)
+        {
+            shipXpos = 0;
+        }
     }
 
     public void paintSelf(Graphics2D g2)
     {
+        g2.drawString("Course " + shipHeading, 1800, 200);
+        g2.drawString("Speed " + shipSpeed, 1800, 300);
         g2.setStroke(new BasicStroke(.01f));
         g2.setColor(Color.BLUE);
         g2.translate(shipXpos, shipYpos);
         g2.scale(20, 20);
         g2.rotate(Math.toRadians(shipHeading));
         g2.draw(shipShape);
+        g2.setTransform(new AffineTransform());
     }
 }
